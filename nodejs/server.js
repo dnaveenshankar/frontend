553d845c84f7e97edfcbdc6ce0914758cc8cdb1c
Naveen Shankar D <dnaveenshankar2003@gmail.com>
2025-03-08 12:40:45 +0530

    Add demo HTML and Node.js server files; update server response message

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,7 +4,7 @@ const server = http.createServer((req,resp)=>{
 
     resp.statusCode =200;
     resp.setHeader("Content-type", "text/plain")
-    resp.end("Hello World fro NodeJs Server !!");
+    resp.end("Hello from NodeJs Server !!");
 });
 
 server.listen(8000, () =>{
