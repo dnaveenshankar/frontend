547260abf137534769a21ab153113a180aa22af9
Naveen Shankar D <dnaveenshankar2003@gmail.com>
2025-03-08 12:15:17 +0530

    Update server response message and console log for clarity

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,9 +4,9 @@ const server = http.createServer((req,resp)=>{
 
     resp.statusCode =200;
     resp.setHeader("Content-type", "text/plain")
-    resp.end("Hello World !!");
+    resp.end("Hello World fro NodeJs Server !!");
 });
 
 server.listen(8000, () =>{
-    console.log("Server Running in port 8000!!!");
+    console.log("Server Running in port 8000 - http://localhost:8000");
 })
