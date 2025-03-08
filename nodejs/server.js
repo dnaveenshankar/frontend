f39500c6c5dec3fa803f9677c785baf03de48432
Naveen Shankar D <dnaveenshankar2003@gmail.com>
2025-03-08 12:10:59 +0530

    Initialize Node.js project with basic server and addition functionality

---
+++ nodejs/server.js
@@ -0,0 +1,12 @@
+const http=require("http");
+
+const server = http.createServer((req,resp)=>{
+
+    resp.statusCode =200;
+    resp.setHeader("Content-type", "text/plain")
+    resp.end("Hello World !!");
+});
+
+server.listen(8000, () =>{
+    console.log("Server Running in port 8000!!!");
+})
